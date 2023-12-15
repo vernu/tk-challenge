994cadb3f91984d776605c4fc8c1e36130668ba5
isra el <vernu1997@gmail.com>
2023-12-15 20:03:44 +0300

    fix(be): fix cors issue

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"tk-challenge/handlers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -11,6 +12,9 @@ import (
 func main() {
 	godotenv.Load()
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	router.Use(cors.New(config))
 	router.GET("/venues", handlers.SearchVenue)
 	router.Run(":" + os.Getenv("PORT"))
 }
